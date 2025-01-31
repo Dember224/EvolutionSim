8e9f59cbbdda780357cd16bcf2f823ed2e7f6bfc
Antoine D Washington <Dember224@gmail.com>
2025-01-30 19:55:21 -0500

    Adding the mutation logic

--- Organisms/organism.py
+++ Organisms/organism.py
@@ -12,7 +12,7 @@ class Organism:
         self.alive = alive
         self.trait_dict = trait_dict
 
-    def mutate(self, target_trait, mutation_rate: float = 0.1):
+    def mutate(self, target_trait, mutation_rate: float = 0.01):
         """Mutate a trait so that it's different in subsequent generations."""
         mutating_trait = self.trait_dict[target_trait]
         if isinstance(mutating_trait, float):
@@ -27,6 +27,12 @@ class Organism:
                 mutating_trait = not mutating_trait
 
         self.trait_dict[target_trait] = mutating_trait
+
+    def mutate_all_traits(self, mutation_rate: float = 0.01):
+        """Mutate all of the traits in the trait dictionary"""
+        trait_list = list(self.trait_dict.keys)
+        for trait in trait_list:
+            self.mutate(target_trait=trait, mutation_rate=mutation_rate)
 
     def sexual_reproduction(self):
         """Create an offspring of the organism in question"""
